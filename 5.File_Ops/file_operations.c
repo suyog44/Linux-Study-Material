8433a7dcbc7ba4d54f2f74cc47eddd5da373fb24
Suyog B <suyogburadkar@gmail.com>
2024-06-06 00:30:54 +0530

    Chapter 5: Add cdev and device class for char device

--- 5.File_Ops/file_operations.c
+++ 5.File_Ops/file_operations.c
@@ -3,6 +3,8 @@
 #include <linux/module.h>
 #include <linux/fs.h>
 #include <linux/kdev_t.h>
+#include <linux/cdev.h>
+#include <linux/device.h>
 
 static ssize_t file_read(struct file *, char __user *, size_t, loff_t *);
 static ssize_t file_write(struct file *, const char __user *, size_t, loff_t *);
@@ -10,6 +12,8 @@ static int file_open(struct inode *, struct file *);
 static int file_release(struct inode *, struct file *);
 
 dev_t dev;
+static struct cdev cdev_ops;
+static struct class *dev_class;
 
 static struct file_operations fops = {
 	read: file_read,
@@ -20,18 +24,22 @@ static struct file_operations fops = {
 
 
 static ssize_t file_read(struct file *file, char __user *buf, size_t len, loff_t *loff){
+	printk("device file opened for reading");
 	return 0;
 }
 
 static ssize_t file_write(struct file *file, const char __user *buf, size_t len, loff_t *loff){
+	printk("device file opened for writing");
 	return len;
 }
 
 static int file_open(struct inode *inode, struct file *file){
+	printk("device file opened successfully");
 	return 0;
 }
 
 static int file_release(struct inode *inode, struct file *file){
+	printk("device file released");
 	return 0;
 }
 
@@ -42,12 +50,23 @@ static int __init file_operations_init(void)
 		return -1;
 	}
 
+	pr_info("Major:%d Minor:%d\n", MAJOR(dev), MINOR(dev));
+
+	cdev_init(&cdev_ops, &fops);
+
+	if(cdev_add(&cdev_ops, dev, 1)<0){
+		pr_err("cannot create device class");
+		unregister_chrdev_region(dev,1);
+		return -1;
+	}
+
+
 	return 0;
 }
 
 static void __exit file_operations_exit(void)
 {
-	unregister_chrdev_region(&dev, 1);
+	unregister_chrdev_region(dev, 1);
 }
 
 module_init(file_operations_init);
